046d2259cfcc0d33e6e302e5c0ec338f7f15c7c3
Natanael Arndt <arndtn@gmail.com>
2024-11-13 22:17:04 +0100

    Cast resource list

--- rdflib_jinja/resource.py
+++ rdflib_jinja/resource.py
@@ -1,8 +1,12 @@
-from rdflib.resource import Resource as RDFLibResource
+from rdflib.resource import Resource as RDFResource
 from rdflib.util import from_n3
 
-class Resource(RDFLibResource):
+class Resource(RDFResource):
     def __getitem__(self, item):
         if isinstance(item, str):
-            return super().__getitem__(from_n3(item))
-        return super().__getitem__(item)
+            return self._cast_list(super().__getitem__(from_n3(item)))
+        return self._cast_list(super().__getitem__(item))
+
+    def _cast_list(self, list):
+        for item in list:
+            yield self._cast(item)
