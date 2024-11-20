cae440a06523183fbe929937fcf298e52dbe1f62
Natanael Arndt <arndtn@gmail.com>
2024-11-20 21:20:05 +0100

    Identifiers are subclasses of str :-\

--- jinja_rdf/rdf_property.py
+++ jinja_rdf/rdf_property.py
@@ -4,12 +4,12 @@ from rdflib.util import from_n3
 
 
 def rdf_property(resource: RDFLibResource, property: str | URIRef):
-    if isinstance(property, str):
+    if isinstance(property, str) and not isinstance(property, URIRef):
         property = from_n3(property)
     return resource.objects(property)
 
 
 def rdf_inverse_property(resource: RDFLibResource, property: str | URIRef):
-    if isinstance(property, str):
+    if isinstance(property, str) and not isinstance(property, URIRef):
         property = from_n3(property)
     return resource.subjects(property)
