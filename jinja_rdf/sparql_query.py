dddd9c57d7e083f1fed5e01f0370a88b922152f9
Natanael Arndt <arndtn@gmail.com>
2025-03-27 15:23:35 +0100

    use jinja context and namespaces

--- jinja_rdf/sparql_query.py
+++ jinja_rdf/sparql_query.py
@@ -1,17 +1,27 @@
+from rdflib import URIRef
 from rdflib.resource import Resource as RDFLibResource
 from rdflib import Graph
+from jinja2 import pass_context
+from jinja2.runtime import Context
 
 
-def sparql_query(input: RDFLibResource | Graph, query: str):
+@pass_context
+def sparql_query(context: Context, input: RDFLibResource | Graph | URIRef, query: str):
     if isinstance(input, Graph):
         graph = input
+        resourceIri = input.identifier
     if isinstance(input, RDFLibResource):
         graph = input.graph
+        resourceIri = input.identifier
+    if isinstance(input, URIRef):
+        graph = context["graph"]
+        resourceIri = input
     return graph.query(
         query,
         initBindings={
-            "resourceIri": input.identifier,
-            "resourceUri": input.identifier,
+            "resourceIri": resourceIri,
+            "resourceUri": resourceIri,
             "graphIri": graph.identifier,
         },
+        initNs=dict(context["namespace_manager"].namespaces()),
     )
