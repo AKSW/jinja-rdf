ca0825f29283189c106eda1006e965723dc6041d
Natanael Arndt <arndtn@gmail.com>
2025-04-13 00:58:14 +0200

    Define function to register the jinja-rdf filters on an environment

--- jinja_rdf/__init__.py
+++ jinja_rdf/__init__.py
@@ -1,1 +1,15 @@
-from .rdf_property import rdf_property
+from .rdf_property import (
+    rdf_properties,
+    rdf_inverse_properties,
+    rdf_property,
+    rdf_inverse_property,
+)
+from .sparql_query import sparql_query
+
+def register_filters(environment):
+    """Register all jinja-rdf filters on a jinja environment."""
+    environment.filters["properties"] = rdf_properties
+    environment.filters["properties_inv"] = rdf_inverse_properties
+    environment.filters["property"] = rdf_property
+    environment.filters["property_inv"] = rdf_inverse_property
+    environment.filters["query"] = sparql_query
